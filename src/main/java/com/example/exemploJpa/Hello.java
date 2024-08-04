36b3399849e13156efd17ab18dcbd9efd2f162f6
axpedro <pedruax@hotmail.com>
2024-08-04 17:52:43 +0200

    Subindo repo

---
+++ src/main/java/com/example/exemploJpa/Hello.java
@@ -0,0 +1,12 @@
+package com.example.exemploJpa;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+//@NoArgsConstructor
+@AllArgsConstructor
+public class Hello {
+
+}
---
+++ src/test/java/com/example/exemploJpa/ExemploJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemploJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
